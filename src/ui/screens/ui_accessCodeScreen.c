214606103455eae824a63657d9edaa86dae4c41c
Pedro Casaubon <xperiments@gmail.com>
2023-09-06 19:43:55 +0200

    Added switch keyboard

--- src/ui/screens/ui_accessCodeScreen.c
+++ src/ui/screens/ui_accessCodeScreen.c
@@ -2,6 +2,7 @@
 void ui_event_accessCodeScreenSubmit(lv_event_t *e)
 {
     lv_event_code_t code = lv_event_get_code(e);
+
     if (code == LV_EVENT_READY)
     {
         LV_LOG_USER("LV_EVENT_READY");
@@ -15,6 +16,7 @@ void ui_event_accessCodeScreenSubmit(lv_event_t *e)
             lv_msg_send(XTOUCH_ON_CODE_ENTERED, NULL);
         }
     }
+
     if (code == LV_EVENT_VALUE_CHANGED)
     {
         const char *pepe = lv_textarea_get_text(ui_accessCodeInput);
@@ -22,6 +24,16 @@ void ui_event_accessCodeScreenSubmit(lv_event_t *e)
         int n = strlen(pepe);
 
         lv_obj_set_style_text_color(ui_accessCodeInput, n != 8 ? lv_color_hex(0xFF0000) : lv_color_hex(0x0000ff), LV_PART_MAIN | LV_STATE_DEFAULT);
+    }
+
+    if (code == LV_EVENT_CANCEL)
+    {
+        LV_LOG_USER("LV_EVENT_CANCEL");
+        lv_keyboard_mode_t lv_keyboard_mode = lv_keyboard_get_mode(ui_accessCodeScreenKeyboard);
+        bool isKeyboardNumber = lv_keyboard_mode == LV_KEYBOARD_MODE_NUMBER;
+        lv_keyboard_set_mode(ui_accessCodeScreenKeyboard, isKeyboardNumber ? LV_KEYBOARD_MODE_TEXT_LOWER : LV_KEYBOARD_MODE_NUMBER);
+        lv_obj_set_height(ui_accessCodeScreenKeyboard, isKeyboardNumber ? 120 : 190);
+        lv_obj_set_y(ui_accessCodeInput, isKeyboardNumber ? 70 : 0);
     }
 }
 void ui_accessCodeScreen_screen_init(void)
@@ -37,12 +49,12 @@ void ui_accessCodeScreen_screen_init(void)
     lv_obj_set_style_pad_bottom(ui_accessCodeScreen, 0, LV_PART_MAIN | LV_STATE_DEFAULT);
     lv_obj_set_style_pad_row(ui_accessCodeScreen, 0, LV_PART_MAIN | LV_STATE_DEFAULT);
     lv_obj_set_style_pad_column(ui_accessCodeScreen, 0, LV_PART_MAIN | LV_STATE_DEFAULT);
-    lv_obj_set_style_bg_color(ui_accessCodeScreen, lv_color_hex(0x000000), LV_PART_ITEMS | LV_STATE_DEFAULT);
+    lv_obj_set_style_bg_color(ui_accessCodeScreen, lv_color_hex(0x000000), LV_PART_MAIN | LV_STATE_DEFAULT);
 
     // lv_obj_t *ui_accessCodeScreenKeyboard;
     ui_accessCodeScreenKeyboard = lv_keyboard_create(ui_accessCodeScreen);
-    lv_keyboard_set_mode(ui_accessCodeScreenKeyboard, LV_KEYBOARD_MODE_TEXT_LOWER);
-    lv_obj_set_height(ui_accessCodeScreenKeyboard, 120);
+    lv_keyboard_set_mode(ui_accessCodeScreenKeyboard, LV_KEYBOARD_MODE_NUMBER);
+    lv_obj_set_height(ui_accessCodeScreenKeyboard, 190);
     lv_obj_set_width(ui_accessCodeScreenKeyboard, lv_pct(100));
     lv_obj_add_flag(ui_accessCodeScreenKeyboard, LV_OBJ_FLAG_FLOATING);                                                                                                                                                                                                                               /// Flags
     lv_obj_clear_flag(ui_accessCodeScreenKeyboard, LV_OBJ_FLAG_CLICKABLE | LV_OBJ_FLAG_PRESS_LOCK | LV_OBJ_FLAG_CLICK_FOCUSABLE | LV_OBJ_FLAG_GESTURE_BUBBLE | LV_OBJ_FLAG_SNAPPABLE | LV_OBJ_FLAG_SCROLLABLE | LV_OBJ_FLAG_SCROLL_ELASTIC | LV_OBJ_FLAG_SCROLL_MOMENTUM | LV_OBJ_FLAG_SCROLL_CHAIN); /// Flags
@@ -56,7 +68,7 @@ void ui_accessCodeScreen_screen_init(void)
     lv_obj_set_width(ui_accessCodeInput, lv_pct(100));
     lv_obj_set_height(ui_accessCodeInput, LV_SIZE_CONTENT); /// 48
     lv_obj_set_x(ui_accessCodeInput, 0);
-    lv_obj_set_y(ui_accessCodeInput, 70);
+    lv_obj_set_y(ui_accessCodeInput, 0);
     lv_textarea_set_placeholder_text(ui_accessCodeInput, "Enter Access Code");
     lv_textarea_set_one_line(ui_accessCodeInput, true);
     lv_obj_add_flag(ui_accessCodeInput, LV_OBJ_FLAG_FLOATING);                                 /// Flags
