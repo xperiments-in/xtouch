02a9ebda7502d16d1d5bfda64c6d9523a7296d48
Pedro Casaubon <xperiments@gmail.com>
2023-09-08 13:03:38 +0200

    Decrease FrameRate

--- resources/lv_conf.h
+++ resources/lv_conf.h
@@ -78,10 +78,10 @@
  *====================*/
 
 /*Default display refresh period. LVG will redraw changed areas with this period time*/
-#define LV_DISP_DEF_REFR_PERIOD 32      /*[ms]*/
+#define LV_DISP_DEF_REFR_PERIOD 40      /*[ms]*/
 
 /*Input device read period in milliseconds*/
-#define LV_INDEV_DEF_READ_PERIOD 32     /*[ms]*/
+#define LV_INDEV_DEF_READ_PERIOD 40     /*[ms]*/
 
 /*Use a custom tick source that tells the elapsed time in milliseconds.
  *It removes the need to manually update the tick with `lv_tick_inc()`)*/
