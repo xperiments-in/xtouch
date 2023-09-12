83eefd99c7aa3571e2968f04dd1d0a52d397ee86
Pedro Casaubon <xperiments@gmail.com>
2023-09-12 23:11:19 +0200

    Update naming

--- src/xtouch/debug.h
+++ src/xtouch/debug.h
@@ -1,11 +1,11 @@
 #ifndef _XLCD_DEBUG
 #define _XLCD_DEBUG
 
-#define XTOUCH_USE_SERIAL true
+#define XTOUCH_DEBUG_LOG true
 #define XTOUCH_DEBUG_INFO false
 #define XTOUCH_DEBUG_WARNING false
 #define XTOUCH_DEBUG_ERROR false
-#define ConsoleLog if(XTOUCH_USE_SERIAL)Serial
+#define ConsoleLog if(XTOUCH_DEBUG_LOG)Serial
 #define ConsoleInfo if(XTOUCH_DEBUG_INFO)Serial
 #define ConsoleWarn if(XTOUCH_DEBUG_WARNING)Serial
 #define ConsoleError if(XTOUCH_DEBUG_ERROR)Serial
