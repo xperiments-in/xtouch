dbdb85a18e348d56e82c11f2c0ebec1b06512e11
Pedro Casaubon <xperiments@gmail.com>
2023-09-27 14:01:42 +0200

    Updated hms download script

--- scripts/download-hms.js
+++ scripts/download-hms.js
@@ -2,29 +2,54 @@ const fs = require('fs');
 const https = require('https');
 
 const url = 'https://e.bambulab.com/query.php?lang=en';
-const hmsJsonFile = 'resources/hms.json';
-const deviceErrorFile = 'resources/device_error';
-const hmsErrorFile = 'resources/hms_error';
+const hmsDataFileC = 'src/xtouch/bbl/bbl-errors.c';
+const hmsDataFileH = 'src/xtouch/bbl/bbl-errors.h';
 
 
-const getFlatRecordsKeys = (root, varName) => {
+const message_containing_retry = [
+    "07018004",
+    "07018005",
+    "07018006",
+    "07018006",
+    "07018007",
+    "07008012",
+    "07018012",
+    "07028012",
+    "07038012",
+    "07FF8003",
+    "07FF8004",
+    "07FF8005",
+    "07FF8006",
+    "07FF8007",
+    "07FF8010",
+    "07FF8011",
+    "07FF8012",
+    "07FF8013",
+];
+const message_containing_done = [
+    "07FF8007",
+    "12FF8007"
+];
+
+
+const getFlatRecordsKeysC = (root, varName) => {
     return `const char *${varName}[] PROGMEM = {${JSON.stringify(root.map((e) => String(e.ecode)), null, 2).slice(1, -1)}};`;
 }
-const getFlatRecordsValues = (root, varName = 0) => {
+const getFlatRecordsKeysH = (root, varName) => {
+    return `extern const char *${varName}[] PROGMEM;`;
+}
+const getFlatRecordsValuesC = (root, varName = 0) => {
     return `const char *${varName}[] PROGMEM = {${JSON.stringify(root.map((e) => String(e.intro)), null, 2).slice(1, -1)}};`;
 }
-
-const getKeyValueLength = (root, varName) => {
-    return `uint16_t ${varName} = ${root.length};`;
+const getFlatRecordsValuesH = (root, varName = 0) => {
+    return `extern const char *${varName}[] PROGMEM;`;
 }
 
-// Check the local file's modification time
-let localModTime = null;
-try {
-    const stats = fs.statSync(hmsJsonFile);
-    localModTime = stats.mtime.toUTCString();
-} catch (err) {
-    // File doesn't exist locally
+const getKeyValueLengthC = (root, varName) => {
+    return `int ${varName} = ${root.length};`;
+}
+const getKeyValueLengthH = (root, varName) => {
+    return `extern int ${varName};`;
 }
 
 
@@ -43,17 +68,57 @@ https.get(url, (response) => {
             const device_hms = dataObject.data.device_hms.en;
             const device_error = dataObject.data.device_error.en;
 
-            const output = [
-                getKeyValueLength(device_hms, "hms_error_length"),
-                getFlatRecordsKeys(device_hms, "hms_error_keys"),
-                getFlatRecordsValues(device_hms, "hms_error_values"),
-                getKeyValueLength(device_error, "device_error_length"),
-                getFlatRecordsKeys(device_error, "device_error_keys"),
-                getFlatRecordsValues(device_error, "device_error_values")
+            const outputC = [
+                `
+#include <pgmspace.h>
+`,
+                getKeyValueLengthC(device_hms, "hms_error_length"),
+                getFlatRecordsKeysC(device_hms, "hms_error_keys"),
+                getFlatRecordsValuesC(device_hms, "hms_error_values"),
+                getKeyValueLengthC(device_error, "device_error_length"),
+                getFlatRecordsKeysC(device_error, "device_error_keys"),
+                getFlatRecordsValuesC(device_error, "device_error_values"),
+                `int message_containing_retry_total = ${message_containing_retry.length};`,
+                `int message_containing_done_total = ${message_containing_done.length};`,
+                `const char *message_containing_retry[] PROGMEM = {
+${JSON.stringify(message_containing_retry, null, 2).slice(1, -1)}
+};
+const char *message_containing_done[] PROGMEM = {
+${JSON.stringify(message_containing_done, null, 2).slice(1, -1)}
+};`
             ].join('\n\n');
 
-            fs.writeFileSync(hmsJsonFile, data, 'utf8');
-            fs.writeFileSync(hmsErrorFile + '.key.db', output);
+
+            fs.writeFileSync(hmsDataFileC, outputC);
+
+
+            const outputH = [
+                `#ifndef _XLCD_BBL_ERRORS
+#define _XLCD_BBL_ERRORS
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include <pgmspace.h>`,
+                getKeyValueLengthH(device_hms, "hms_error_length"),
+                getFlatRecordsKeysH(device_hms, "hms_error_keys"),
+                getFlatRecordsValuesH(device_hms, "hms_error_values"),
+                getKeyValueLengthH(device_error, "device_error_length"),
+                getFlatRecordsKeysH(device_error, "device_error_keys"),
+                getFlatRecordsValuesH(device_error, "device_error_values"),
+                `extern int message_containing_retry_total;`,
+                `extern int message_containing_done_total;`,
+                `extern const char *message_containing_retry[] PROGMEM;`,
+                `extern const char *message_containing_done[] PROGMEM;`,
+                `#ifdef __cplusplus
+}
+#endif
+
+#endif`
+            ].join('\n\n');
+            fs.writeFileSync(hmsDataFileH, outputH);
 
         });
     } else {
