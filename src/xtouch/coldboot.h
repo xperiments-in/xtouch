681ba703b0c8588ef593e5489b29ce92618c161b
Pedro Casaubon <xperiments@gmail.com>
2023-09-28 10:44:25 +0200

    Fix build error

--- src/xtouch/coldboot.h
+++ src/xtouch/coldboot.h
@@ -19,26 +19,6 @@
 
 // https://github.com/espressif/arduino-esp32/blob/master/libraries/ESP32/examples/ResetReason
 
-enum ResetReason
-{
-    POWERON_RESET = 1,      /**<1,  Vbat power on reset*/
-    SW_RESET = 3,           /**<3,  Software reset digital core*/
-    OWDT_RESET,             /**<4,  Legacy watch dog reset digital core*/
-    DEEPSLEEP_RESET,        /**<5,  Deep Sleep reset digital core*/
-    SDIO_RESET,             /**<6,  Reset by SLC module, reset digital core*/
-    TG0WDT_SYS_RESET,       /**<7,  Timer Group0 Watch dog reset digital core*/
-    TG1WDT_SYS_RESET,       /**<8,  Timer Group1 Watch dog reset digital core*/
-    RTCWDT_SYS_RESET,       /**<9,  RTC Watch dog Reset digital core*/
-    INTRUSION_RESET,        /**<10, Instrusion tested to reset CPU*/
-    TGWDT_CPU_RESET,        /**<11, Time Group reset CPU*/
-    SW_CPU_RESET,           /**<12, Software reset CPU*/
-    RTCWDT_CPU_RESET,       /**<13, RTC Watch dog Reset CPU*/
-    EXT_CPU_RESET,          /**<14, for APP CPU, reseted by PRO CPU*/
-    RTCWDT_BROWN_OUT_RESET, /**<15, Reset when the vdd voltage is not stable*/
-    RTCWDT_RTC_RESET,       /**<16, RTC Watch dog reset digital core and rtc module*/
-
-};
-
 #include <Arduino.h>
 
 void xtouch_coldboot_check()
